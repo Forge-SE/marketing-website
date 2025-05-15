3cdf3f83c79ba3aa388d207b7f085eff22d13ba0
Selasie5 <selasisepenu5@gmail.com>
2025-05-15 16:33:14 +0000

    Add SVG graphics for stair and triangle groups
    - Created a new SVG file for the stair group with multiple rectangles and a path, enhancing the visual representation of a staircase.
    - Added a new SVG file for the triangle group featuring layered triangles in various colors, contributing to the overall design elements of the project.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const manrope = Manrope({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${manrope.variable} antialiased`}
       >
         {children}
       </body>
